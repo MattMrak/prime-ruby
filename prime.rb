e31e72143953bc7637cc1b3dc6f741af85108b74
MattMrak <matthewmrakovcic@gmail.com>
2020-12-11 21:24:35 -0500

    commit tree 297c96853441b5592d8a2d99b81675dfe947cc9e

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(value)
+    if value <= 1 || value == 0 || value == 1
+        return false
+    elsif
+        (2..value - 1).each do |z|
+            if value % z == 0
+                return false
+            end
+        end
+    end
+    true
+end
